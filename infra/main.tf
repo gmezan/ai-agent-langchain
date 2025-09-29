b78781b881adcdcef92e8097a1382fa784f93de1
gmezan <meza.gustavo@pucp.edu.pe>
2025-09-29 18:42:56 -0500

    feat(infra): infra set up and rg

---
+++ infra/main.tf
@@ -0,0 +1,9 @@
+resource "random_pet" "rg_name" {
+  prefix = "rg"
+}
+
+module "resource_group" {
+  source = "git::https://github.com/gmezan/terraform-infra-gh-actions.git//module/azure/rg?ref=main-azure"
+  name     = random_pet.rg_name.id
+  location = var.location
+}
